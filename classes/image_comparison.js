b163a67f2ea308576a62632146b5246d93804f2d
Hugo A. Gaxiola <hugoagaxiola@gmail.com>
2022-02-09 23:00:49 -0800

    Set partial functionality for image comparisons

--- classes/image_comparison.js
+++ classes/image_comparison.js
@@ -1,19 +1,73 @@
+import resemble from "resemblejs";
+import fs from "fs";
 class ImageComparison {
-    constructor (params) {
+  _defaultConfig = {
+    output: {
+      errorColor: {
+        red: 255,
+        green: 0,
+        blue: 255,
+      },
+      errorType: "movement",
+      transparency: 0.3,
+      largeImageThreshold: 1200,
+      useCrossOrigin: false,
+      outputDiff: true,
+    },
+    scaleToSameSize: true,
+    ignore: "antialiasing",
+  };
 
+  constructor(params) {
+    Object.assign(this, params);
+  }
+
+  async _getDirectoryFiles(path) {
+    return await fs.promises.readdir(path).then((fileNames) => {
+      return fileNames.filter(
+        (file) => file.includes(".png") || file.includes(".jpg")
+      );
+    });
+  }
+
+  async findMatchingFiles() {
+    let imageComparison = this;
+    let paths = await Promise.all([
+      this._getDirectoryFiles(this.currentPath),
+      this._getDirectoryFiles(this.controlPath),
+    ]);
+
+    await this._handleImageComparison(paths);
+  }
+
+  async _handleImageComparison(imageDirectories) {
+    let imageComparison = this;
+    let controlData = imageDirectories[0];
+    let newData = imageDirectories[1];
+
+    var intersections = controlData.filter(e => newData.indexOf(e) !== -1);
+
+    console.log('INTERSECTIONS', intersections);
+    
+    for (const match of intersections) {
+      await imageComparison.compareImagePairs(
+        imageComparison.controlPath + "/" + match,
+        imageComparison.currentPath + "/" + match,
+        match);
     }
-    getControlDirectory() {
+  }
 
-    }
+  async compareAllImages() {}
 
-    getCurrentDirectory() {
-
-    }
-
-    findMatchingFiles() {
-        
-    }
-
+  async compareImagePairs(baseImage, newImage, filename) {
+    const data = resemble(baseImage)
+    .compareTo(newImage)
+    .onComplete((data) => {
+      console.log(data);
+      fs.promises.writeFile(filename + '.png', data.getBuffer())
+      // fs.writeFile(filename + '.png', data.getBuffer());
+    })
+  }
 }
 
 export default ImageComparison;
--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // External Dependencies
-import puppeteer from 'puppeteer';
-import fs from 'fs';
-import lighthouse from 'lighthouse';
-import chalk from 'chalk';
-import ora from 'ora';
-import resemble from 'resemblejs';
+import puppeteer from "puppeteer";
+import fs from "fs";
+import lighthouse from "lighthouse";
+import chalk from "chalk";
+import ora from "ora";
+import resemble from "resemblejs";
 
 // Internal Modules
-import Test from './classes/test.js';
-import ResultImages from './classes/result_images.js';
-import LighthouseTest from './classes/lighthouse_test.js';
-import Summary from './classes/summary.js';
+import Test from "./classes/test.js";
+import ResultImages from "./classes/result_images.js";
+import LighthouseTest from "./classes/lighthouse_test.js";
+import Summary from "./classes/summary.js";
+import ImageComparison from "./classes/image_comparison.js";
 
 const config = JSON.parse(fs.readFileSync(process.argv.slice(2)[0]));
 const testsToRun = config.tests;
@@ -18,144 +19,186 @@ const testsToRun = config.tests;
 runAllTests(testsToRun);
 
 async function createBrowserInstance(config, curr, total) {
-    let cwv, resultImagesInstance, lighthouseInstance; 
-    const testSpinner = ora({
-        prefixText:`(${curr}/${total})`,
-        text: `Creating ${chalk.gray('puppeteer.js browser instance')}...`,
-        color: 'white',
-        spinner: 'dots',
-    }).start();
+  let cwv, resultImagesInstance, lighthouseInstance;
+  const testSpinner = ora({
+    prefixText: `(${curr}/${total})`,
+    text: `Creating ${chalk.gray("puppeteer.js browser instance")}...`,
+    color: "white",
+    spinner: "dots",
+  }).start();
 
-    const browser = await puppeteer.launch({
-        ignoreDefaultArgs: ['--disable-extensions'],
-        devtools: false
+  const browser = await puppeteer.launch({
+    ignoreDefaultArgs: ["--disable-extensions"],
+    devtools: false,
+  });
+
+  const page = await browser.newPage();
+  await page.setViewport(config.viewport);
+  await page.authenticate(config.authenticate);
+  await page.goto(config.url);
+
+  testSpinner
+    .stopAndPersist({
+      symbol: `${chalk.hex("#33C7FF")("✓")}`,
+      text: `Created ${chalk.hex("#33C7FF")(
+        "puppeteer.js browser instance"
+      )}...`,
+    })
+    .start();
+
+  if (config.screenshots !== undefined) {
+    testSpinner.text = `Creating ${chalk.gray("result images")}...`;
+
+    resultImagesInstance = new ResultImages({
+      page: page,
+      name: config.name,
+      screenshots: config.screenshots,
     });
-        
-    const page = await browser.newPage();
-    await page.setViewport(config.viewport);
-    await page.authenticate(config.authenticate);
-    await page.goto(config.url);
+    await resultImagesInstance.createTestImages();
 
     testSpinner.stopAndPersist({
-        symbol: `${chalk.hex('#33C7FF')('✓')}`,
-        text: `Created ${chalk.hex('#33C7FF')('puppeteer.js browser instance')}...`,
-    }).start();
+      symbol: `${chalk.hex("#33C7FF")("✓")}`,
+      text: `Created ${chalk.hex("#33C7FF")("result images")}...`,
+      spinner: "dots",
+    });
+  }
 
-    if (config.screenshots !== undefined) {
-        testSpinner.text = `Creating ${chalk.gray('result images')}...`;
+  if (config.lighthouseConfig !== undefined) {
+    const lighthouseFlags = {
+      port: new URL(browser.wsEndpoint()).port,
+      output: "json",
+      logLevel: "silent",
+      maxWaitForLoad: 60 * 1000,
+    };
 
-        resultImagesInstance = new ResultImages({
-            page: page, 
-            name: config.name,
-            screenshots: config.screenshots
-        });
-        await resultImagesInstance.createTestImages();
-        
-        testSpinner.stopAndPersist({
-            symbol: `${chalk.hex('#33C7FF')('✓')}`,
-            text: `Created ${chalk.hex('#33C7FF')('result images')}...`,
-            spinner: 'dots',
-        });
-    }
+    lighthouseInstance = new LighthouseTest({
+      name: config.name,
+      url: config.url,
+      lighthouse: lighthouse,
+      browser: browser,
+      outputsJson: config.lighthouseOutputsJson,
+      config: config.lighthouseConfig,
+      flags: lighthouseFlags,
+    });
 
-    if (config.lighthouseConfig !== undefined) {
-        const lighthouseFlags = {
-            port: (new URL(browser.wsEndpoint())).port,
-            output: 'json',
-            logLevel: 'silent',
-            maxWaitForLoad: 60 * 1000
-        }
-    
-        lighthouseInstance = new LighthouseTest({
-            name: config.name,
-            url: config.url,
-            lighthouse: lighthouse,
-            browser: browser,
-            outputsJson: config.lighthouseOutputsJson,
-            config: config.lighthouseConfig,
-            flags: lighthouseFlags
-        });
-    
-        testSpinner.text =`Running ${chalk.gray('lighthouse report')}...`;
-        testSpinner.start();
-        
-        await lighthouseInstance.runInstance().then((data) => cwv = data);
-        
-        testSpinner.stopAndPersist({
-            symbol: `${chalk.hex('#33C7FF')('✓')}`,
-            text: `Completed ${chalk.hex('#33C7FF')('lighthouse report')}...`,
-            spinner: 'dots',
-        });
-    }
-    
-    await browser.close();
+    testSpinner.text = `Running ${chalk.gray("lighthouse report")}...`;
+    testSpinner.start();
+
+    await lighthouseInstance.runInstance().then((data) => (cwv = data));
 
     testSpinner.stopAndPersist({
-        symbol: `${chalk.hex('#1BFF00')('✓')}`,
-        text: `Completed ${chalk.hex('#1BFF00')('test run\n\n')}`,
-        spinner: 'dots',
+      symbol: `${chalk.hex("#33C7FF")("✓")}`,
+      text: `Completed ${chalk.hex("#33C7FF")("lighthouse report")}...`,
+      spinner: "dots",
     });
-    
-    testSpinner.clear().stop();
+  }
 
-    return cwv;
+  await browser.close();
+
+  testSpinner.stopAndPersist({
+    symbol: `${chalk.hex("#1BFF00")("✓")}`,
+    text: `Completed ${chalk.hex("#1BFF00")("test run\n\n")}`,
+    spinner: "dots",
+  });
+
+  testSpinner.clear().stop();
+
+  return cwv;
 }
 
 async function runSingleTest(test, currentNum, total) {
-    let currentTest, result = {};
-    test.fs = fs;
-    currentTest = new Test(test);
+  let currentTest,
+    result = {};
+  test.fs = fs;
+  currentTest = new Test(test);
 
-    console.log(chalk.hex('#FFC300')(`Starting Test ---> ${currentTest.name}`));
-    console.log(chalk.hex('#00FEC4').italic('results will be output to... ./results/' + currentTest.name + '\n'));
+  let lala = new ImageComparison({
+    controlPath: "./results/screenshots-test(8-2-2022 22H16M3)",
+    currentPath: "./results/screenshots-test(9-2-2022 17H56M45)",
+  }).findMatchingFiles();
+  
+  console.log(chalk.hex("#FFC300")(`Starting Test ---> ${currentTest.name}`));
+  console.log(
+    chalk
+      .hex("#00FEC4")
+      .italic(
+        "results will be output to... ./results/" + currentTest.name + "\n"
+      )
+  );
 
-    return await createBrowserInstance(currentTest, currentNum, total).then((output) => {
-        result.lighthouse = output;
-        result.name = currentTest.name;
-        
-        return result;
-    });
+  return await createBrowserInstance(currentTest, currentNum, total).then(
+    (output) => {
+      result.lighthouse = output;
+      result.name = currentTest.name;
+
+      return result;
+    }
+  );
 }
 
 async function runAllTests(testsToRun) {
-    let counter = 1;
-    console.log(chalk.hex('#FF00AE')('\nGeppetto.js v1.0') + ' - Initialized');
-    console.log(chalk.white(`${testsToRun.length} tests will be run\n`));
+  let counter = 1;
+  console.log(chalk.hex("#FF00AE")("\nGeppetto.js v1.0") + " - Initialized");
+  console.log(chalk.white(`${testsToRun.length} tests will be run\n`));
 
-    var geppettoSummary = new Summary({});
-    
-    for (const test of testsToRun) {
-        await runSingleTest(test, counter, testsToRun.length).then(res => {
-            geppettoSummary.createSingleTestSummary(res.name, res.lighthouse);
-        });
-        counter++;
+  var geppettoSummary = new Summary({});
+
+  for (const test of testsToRun) {
+    await runSingleTest(test, counter, testsToRun.length).then((res) => {
+      geppettoSummary.createSingleTestSummary(res.name, res.lighthouse);
+    });
+    counter++;
+  }
+
+  console.log(chalk.hex("#FF6725")("\nAll Tests CWVs Summary"));
+
+  let summaryData = geppettoSummary.createDataSummary();
+  fs.writeFile(
+    "summarized_geppetto_run.json",
+    JSON.stringify(summaryData),
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(
+          chalk
+            .hex("#00FEC4")
+            .italic("Summary Data JSON saved on ./summarized_geppetto_run.json")
+        );
+      }
     }
+  );
 
-    console.log(chalk.hex('#FF6725')('\nAll Tests CWVs Summary'));
+  fs.writeFile(
+    "all_tests_data.json",
+    JSON.stringify(geppettoSummary.getResultsData()),
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(
+          chalk
+            .hex("#00FEC4")
+            .italic("All Tests Data JSON saved on ./all_tests_data.json")
+        );
+      }
+    }
+  );
 
-    let summaryData = geppettoSummary.createDataSummary();
-    fs.writeFile('summarized_geppetto_run.json', JSON.stringify(summaryData),(err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(chalk.hex('#00FEC4').italic('Summary Data JSON saved on ./summarized_geppetto_run.json'));
-    }});
-    
-    fs.writeFile('all_tests_data.json', JSON.stringify(geppettoSummary.getResultsData()),(err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(chalk.hex('#00FEC4').italic('All Tests Data JSON saved on ./all_tests_data.json'));
-        }
-    });
+  Object.keys(summaryData).forEach((section) => {
+    let name = section
+      .split("-")
+      .map((str) => {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+      })
+      .join(" ");
 
-    Object.keys(summaryData).forEach(section => {
-        let name = section.split('-').map(str => {
-            return str.charAt(0).toUpperCase() + str.slice(1)
-        }).join(' ');
+    console.log(
+      chalk.white(name) +
+        " | mean: " +
+        chalk.hex("#33C7FF")(summaryData[section].mean)
+    );
+  });
 
-        console.log(chalk.white(name) + ' | mean: ' + chalk.hex('#33C7FF')(summaryData[section].mean));
-    });
-
-    console.log(chalk.hex('#FF00AE')('\nGeppetto.js v1.0') + ' - Ended\n');
+  console.log(chalk.hex("#FF00AE")("\nGeppetto.js v1.0") + " - Ended\n");
 }
